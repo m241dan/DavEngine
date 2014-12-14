edc8ccfaa5fc803bc1643ee7e3b5ff6ea8eb8198
Davenge <korisd@gmail.com>
2014-12-14 13:09:46 -0500

    fixed a bug in element_info.sql

--- sql/element_info.sql
+++ sql/element_info.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `element_info` (
    `element` varchar(255) NOT NULL DEFAULT ' ',
    `pen` int NOT NULL DEFAULT '0',
    `res` int NOT NULL DEFAULT '0',
-   `potency` int NOT NULL DEFAULT '0',
+   `potency` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `compositions` ADD UNIQUE `element_info` ( `owner`, `element` );
+ALTER TABLE `element_info` ADD UNIQUE `unique_index` ( `owner`, `element` );
